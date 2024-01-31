163cc302d8dd6696f609de25eda8aab88b4d5215
velikhawk <velikerlangga@gmail.com>
2024-01-31 07:53:42 +0700

    pengembangan

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function openModal(imageSrc) {
+    document.getElementById('modal').style.display = "block";
+    document.getElementById("img01").src = imageSrc;
+  }
+   
+  function closeModal() {
+    document.getElementById('modal').style.display = "none";
+  }
+  
